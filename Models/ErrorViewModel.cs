8c6ccef48e10cd2c3041e71152dcc589ec5301a0
Emmanuel-Q <78229107+Emmanuel-Q@users.noreply.github.com>
2022-12-12 16:51:18 +0000

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentIdHubApp.MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
